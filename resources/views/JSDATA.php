2845d7ca8ce0fea651f890bed04d1be293ba0c6b
Thomas Zwarts <thomaszwarts@gmail.com>
2018-11-07 16:03:40 +0100

    misc

--- resources/views/JSDATA.php
+++ resources/views/JSDATA.php
@@ -10,7 +10,7 @@
   if (isset($options)) {
       foreach ($options['javascript_data'] as $key => $jsData) {
           if (gettype($jsData) == 'array') {
-              $j = array_map($jsData, $j);
+              $j = array_map('strval', $jsData);
               $val = json_encode(mb_convert_encoding($j, "UTF-8", "UTF-8"), JSON_UNESCAPED_SLASHES);
           } else {
               $val = json_encode(mb_convert_encoding($jsData, "UTF-8", "UTF-8"), JSON_UNESCAPED_SLASHES);
